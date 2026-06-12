f4f3d876978a0bda0ec7ef44fe2efe0f95ff0bdc
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-11 22:43:37 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weatherappg15/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
